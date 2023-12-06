3f9c0831f3471d992068564e01a56d4e5addd833
Morika1 <mor7025@gmail.com>
2023-12-06 13:14:56 +0200

    Initial Commit

---
+++ APIWeatherApp/APIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIWeatherAppApp.swift
+//  APIWeatherApp
+//
+//  Created by Mac on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct APIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
